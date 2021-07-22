137adffaf4fe9acff315cb5b990ce314b1e79b22
mike <apolloproductionsdj@gmail.com>
2021-07-21 23:11:11 -0700

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKnkGP26F2hov4x1wcw4IaqrdZz4B0gXk",
+    authDomain: "netflix-build-a64e9.firebaseapp.com",
+    projectId: "netflix-build-a64e9",
+    storageBucket: "netflix-build-a64e9.appspot.com",
+    messagingSenderId: "2323917772",
+    appId: "1:2323917772:web:1fe5d9fa9ebae17cf60b69"
+};
