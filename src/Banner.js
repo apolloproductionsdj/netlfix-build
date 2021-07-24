fa38cf4b3e76e1defa339bf83e21bbed021d5424
mike <apolloproductionsdj@gmail.com>
2021-07-24 13:17:32 -0700

    finished banner with css and hardcoded info

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './Banner.css';
 
 function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
     return (
         <header className="banner" style={{
             backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
@@ -17,7 +22,10 @@ function Banner() {
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    This is a test description
+                    {truncate(`This is a test description
+                    This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description
+                    This is a test description This is a test description This is a test description This is a test description This is a test description
+                    This is a test description`, 100)}
                 </h1>
             </div>
             <div className="banner--fadeBottom" />
